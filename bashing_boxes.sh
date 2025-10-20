ed3c35630a84410b52f60161e1d87af4609084f4
Mokonji07 <mokonji@smail.pembinatrails.ca>
2025-10-20 09:51:14 -0500

    bashing_boxes

---
+++ bashing_boxes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#nano readme
+# Bashing Boxes
+#===============
+array_name=("mal" "ben" "carlos" "evie" "Jay" "Audrey" "uma" "Hook" "Red" "Chloe")
+
+echo "hello"
+echo "choose an option: "
+read varaiable
+
+echo $varaiable
