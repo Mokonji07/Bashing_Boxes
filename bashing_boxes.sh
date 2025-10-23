c1a35ef25852cb1ee66f1d2c655ba294d81f0b63
Mokonji07 <mokonji@smail.pembinatrails.ca>
2025-10-23 10:46:51 -0500

    Completed Code for bashing_boxes

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -3,22 +3,81 @@
 # Bashing Boxes
 #===============
 array_of_things=("mal" "ben" "carlos" "evie" "Jay" "Audrey" "uma" "Hook" "Red" "Chloe")
+menu_of_possibilities=("1. Print list"
+"2. Print item at X Position in list"
+"3. add item to list"
+"4. remove last item from list"
+"5. remove item from X position"
+"6. exit")
+echo "${menu_of_possibilities[@]}"
+read -p "choose an option [1-6]:" menu
 
-echo "
-	hello
-	This is on a new line!
-	"
-echo $array_of_things
-echo "choose an option: "
-#read varaiable
-#echo $varaiable
+if ! [[ "$menu" =~ ^[0-9]+$ ]]; then
+    echo "That's not an integer! Exiting..."
+    exit 1
+fi 
 
-echo ${array_of_things[@]}
-read character
-echo "welcome" $character ",Lets do some calculating"
+if [[ $menu -lt 1 || $menu -gt 6 ]]; then
+    echo "Number is out of range. Exiting"
+    exit 1
+fi
 
-read -p "Enter first number: " num1
-read -p "Enter second number: " num2
+if [[ $menu -eq 1 ]]; then
+    echo "${array_of_things[@]}"
+    exit 1
+fi
 
-sum=$((num1 + num2))
-echo "Sum: "$sum
+if [[ $menu -eq 2 ]]; then
+      read -p "Enter item position: " pos
+    if [[ "$pos" =~ ^[0-9]+$ ]] && (( pos >= 1 && pos <= ${#array_of_things[@]} )); then
+        echo "${array_of_things[$((pos-1))]}"
+    else
+        echo "invalid number"
+        exit
+    fi
+fi
+
+if [[ $menu -eq 3 ]]; then 
+    read -p "Enter new character" character
+    array_of_things+=("$character")
+    echo "${array_of_things[@]}"
+    exit 1
+fi
+
+if [[ $menu -eq 4  ]]; then
+    echo "DO you want to remove" {$array_of_things[-1]}
+    read -p "Y/n" yes_no
+    if [[ "$yes_no" == "Y" || "$yes_no" == "y" ]]; then
+        unset 'array_of_things[-1]'
+        echo "${array_of_things[@]}"
+        exit
+    elif [[ $yes_no == "N" || "$yes_no" == "n" ]]; then
+        echo "Goodbye..."
+        exit
+    else
+        echo "invalid response"
+        exit
+    fi
+fi
+
+if [[ $menu -eq 5 ]]; then
+    read -p "Enter item position: " pos
+    if [[ "$pos" =~ ^[0-9]+$ ]] && (( pos >= 1 && pos <= ${#array_of_things[@]} )); then
+        echo "You chose: ${array_of_things[$((pos-1))]}"
+        read -p "Remove it? (Yes/no): " yes__no
+        if [[ "$yes__no" == "Yes" || "$yes__no" == "yes" ]]; then
+            unset 'array_of_things[$((pos-1))]'
+            array_of_things=("${array_of_things[@]}")  # reindex
+            echo "Updated list: ${array_of_things[@]}"
+        else
+            echo "Cancelled."
+        fi
+    else
+        echo "Invalid position! Exiting..."
+        exit 1
+    fi
+fi
+
+if [[ $menu -eq 6 ]]; then
+    exit 1
+fi
